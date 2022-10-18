3d149977694516130d9531a2a231c86bbdd8e469
Divyesh Vartha <divyeshvartha@gmail.com>
2022-10-18 19:34:05 +0530

    Now serving bootloader + some more bootstrapifying

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, 'public')));
 
 const { Node, topicList } = require('./models/listNode');
 const mongoose = require('mongoose');
