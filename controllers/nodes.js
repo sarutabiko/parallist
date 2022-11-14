4d35aaeb2ad95654dcfb091d0adfab5a6a12e54e
Divyesh Vartha <divyeshvartha@gmail.com>
2022-11-14 11:09:43 +0530

    add controllers to /node route

---
+++ controllers/nodes.js
@@ -0,0 +1,61 @@
+const { Node } = require('../models/node');
+
+
+module.exports.renderIndex = async (req, res, next) => {
+    let { topic } = req.query;
+    let allNodes;
+    if (topic) { allNodes = await Node.find({ topic }); }
+    else { topic = "all"; allNodes = await Node.find({}); }
+
+    res.render('all', { allNodes, topic, "title": `${topic} Nodes` })
+}
+
+module.exports.createNode = async (req, res) => {
+    req.body.entry = await Node.countDocuments({ topic: req.body.topic }) + 1;
+    // basic error handling
+    // if (!(req.body))
+    //     throw new ExpressError('400', "Invalid Node data received..")
+    const { topic, entry, data } = req.body;
+    const newNode = new Node({ topic, entry, data });
+    await newNode.save();
+    console.log(newNode);
+    res.redirect(`/nodes/${newNode.id}`);
+}
+
+module.exports.renderNewForm = (req, res) => {
+    res.render('new', { 'title': 'Create new node' });
+}
+
+module.exports.showNode = async (req, res) => {
+    const { id } = req.params;
+    const getNode = await Node.findById(id);
+    res.render('showNode', { getNode, 'title': 'Show' });
+}
+
+module.exports.updateNode = async (req, res) => {
+    const { id } = req.params;
+    const getNode = await Node.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+    console.log(getNode);
+    res.redirect(`/nodes/${getNode._id}`);
+}
+
+module.exports.deleteNode = async (req, res) => {
+    const { id } = req.params;
+    const output = await Node.findOneAndDelete({ "_id": id });
+    const list = await Node.find({ "topic": output.topic, "entry": { $gt: output.entry } });
+    for (let i of list) {
+        i.entry--;
+        await i.save();
+    }
+    console.log(output);
+    res.redirect('/nodes');
+}
+
+module.exports.renderEditForm = async (req, res, next) => {
+    const { id } = req.params;
+    const getNode = await Node.findById(id);
+    if (!getNode)
+        return next(new AppError('404', "Node not found. Invalid _id."));
+
+    res.render('edit', { getNode, 'title': 'Edit node', topicList });
+}
--- routes/nodes.js
+++ routes/nodes.js
@@ -1,67 +1,30 @@
 const catchAsync = require("../utils/catchAsync");
 
+const nodes = require('../controllers/nodes');
+
 const express = require('express');
 const router = express.Router();
-const { Node, topicList } = require('../models/listNode');
 const { validateNode } = require('../utils/middleware');
 
-router.get('/', catchAsync(async (req, res, next) => {
-    let { topic } = req.query;
-    let allNodes;
-    if (topic) { allNodes = await Node.find({ topic }); }
-    else { topic = "all"; allNodes = await Node.find({}); }
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
-    res.render('all', { allNodes, topic, "title": `${topic} Nodes` })
-}))
+router.route('/')
+    .get(catchAsync(nodes.renderIndex))
+    // .post(validateNode, catchAsync(nodes.createNode))
+    .post(upload.single('image'), (req, res) => { res.send(console.log(req.body, req.file)) })
 
-router.get('/new', (req, res) => {
-    res.render('new', { 'title': 'Create new node' });
-})
+router.route('/new')
+    .get(nodes.renderNewForm)
 
-router.post('/', validateNode, catchAsync(async (req, res) => {
-    req.body.entry = await Node.countDocuments({ topic: req.body.topic }) + 1;
-    // basic error handling
-    // if (!(req.body))
-    //     throw new ExpressError('400', "Invalid Node data received..")
-    const { topic, entry, data } = req.body;
-    const newNode = new Node({ topic, entry, data });
-    await newNode.save();
-    console.log(newNode);
-    res.redirect(`/nodes/${newNode.id}`);
-}))
 
-router.get('/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const getNode = await Node.findById(id);
-    res.render('showNode', { getNode, 'title': 'Show' });
-}))
+router.route('/:id')
+    .get(catchAsync(nodes.showNode))
+    .put(validateNode, catchAsync(nodes.updateNode))
+    .delete(catchAsync(nodes.deleteNode))
 
-router.get('/:id/edit', catchAsync(async (req, res, next) => {
-    const { id } = req.params;
-    const getNode = await Node.findById(id);
-    if (!getNode)
-        return next(new AppError('404', "Node not found. Invalid _id."));
 
-    res.render('edit', { getNode, 'title': 'Edit node', topicList });
-}))
-
-router.put('/:id', validateNode, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const getNode = await Node.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
-    console.log(getNode);
-    res.redirect(`/nodes/${getNode._id}`);
-}))
-
-router.delete('/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const output = await Node.findOneAndDelete({ "_id": id });
-    const list = await Node.find({ "topic": output.topic, "entry": { $gt: output.entry } });
-    for (let i of list) {
-        i.entry--;
-        await i.save();
-    }
-    console.log(output);
-    res.redirect('/nodes');
-}))
+router.get('/:id/edit', catchAsync(nodes.renderEditForm))
 
 module.exports = router;
